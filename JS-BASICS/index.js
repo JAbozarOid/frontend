2f70a23c0c063cb4fa5a779f82f5116cac9787fb
jabozaroid <araghibdoust@hotmail.com>
2024-06-29 21:46:44 +0330

    adding_js

---
+++ JS-BASICS/index.js
@@ -0,0 +1,12 @@
+// comment --> javascript is for behavior while html is showing for content
+console.log("hello world");
+
+// *** declare variable
+// var is equivlent to var in kotlin and let is eqivalent to val
+// js is case-sensitive
+let username = 'jabozaroid';
+console.log(username);
+
+let firstName = 'abozar', lastName = "raghibdoust";
+
+// *** constants
