4005002bd2d5039af728b3ddc82028506a1ebb46
jabozaroid <araghibdoust@hotmail.com>
2026-01-03 21:03:04 +0330

    login_form

--- React/react-advance/advance/src/App.jsx
+++ React/react-advance/advance/src/App.jsx
@@ -2,6 +2,7 @@ import Products from "./component/Products.jsx";
 import ToDoComponents from "./component/ToDo.jsx";
 import Controlled from "./component/Controlled.jsx";
 import FeedBackForm from "./component/FeedBackForm.jsx";
+import LoginForm from "./component/LoginForm.jsx";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <ToDoComponents/>
         <Controlled/>
         <FeedBackForm/>
+        <LoginForm/>
     </>
   )
 }
